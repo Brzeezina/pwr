e6c370986a5fb2e23df0dd9b5bd9fd08ff8435d6
Brzezina <lopek333@gmail.com>
2022-10-24 11:39:35 +0200

    test

---
+++ param.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int proba = 1, parametr;
+
+void param(){
+    
+}
+
+int main(){
+    printf("Podaj %d. wartosc parametru: ", proba);
+    scanf("%d", &parametr);
+    if(parametr <= 5 && parametr >= -5){
+        printf("Aktualna wartosc parametru: %d\n", parametr);
+    }else if(parametr == 99){
+        return 0;
+    }else printf("Bledna wartosc parametru!\n");
+    proba++;
+    do{
+        printf("Podaj %d. wartosc parametru: ", proba);
+        scanf("%d", &parametr);
+        if(parametr <= 5 && parametr >= -5){
+            printf("Aktualna wartosc parametru: %d\n", parametr);
+        }else if(parametr == 99){
+            return 0;
+        }else printf("Bledna wartosc parametru!\n");
+        proba++;
+    }
+    while(parametr!=99);
+
+    return 0;
+}
+
+//cd OneDrive/Pulpit/studiaC
