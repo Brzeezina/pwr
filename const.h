5ca8551e6af3b2cf2e3c9d94e5f6367efadcbcb9
Brzeezina <110300832+Brzeezina@users.noreply.github.com>
2022-12-15 09:43:26 +0100

    Delete const.h

--- const.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <math.h>
-
-#define MAX 512            /* Maksymalny rozmiar wczytywanego obrazu */
-#define DL_LINII 1024      /* Dlugosc buforow pomocniczych */
